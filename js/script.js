a985dd6fcad587a471c7714881666ea2a6583819
ahaynie1 <95847697+ahaynie1@users.noreply.github.com>
2022-10-13 10:59:08 -0700

    Added confetti
    * added confetti
    * linked JS library
    * set and customized confetti

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
 // My Scripts
 
 console.log('Page Loaded');
+
+// Run it
+let confetti = new Confetti("confetti");
+
+//  Optionalize it
+confetti.destroyTarget(false);
+confetti.setSize(3);
+confetti.setCount(1);
